760cf843178e2a4bdde1ac3bfcb60f60635f7a44
celtlec <maxorangenow@gmail.com>
2020-05-18 20:03:11 +0500

    Updates

---
+++ precache-manifest.4b818a08e6f8b6964acb7fff9170dd65.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41fb5ba0e0bafa11faf30d00bae0b10",
+    "url": "/18/index.html"
+  },
+  {
+    "revision": "34f34588c9dc4e04f189",
+    "url": "/18/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "0401c6e5c9ed093e77bb",
+    "url": "/18/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "34f34588c9dc4e04f189",
+    "url": "/18/static/js/2.c97946ac.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/18/static/js/2.c97946ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0401c6e5c9ed093e77bb",
+    "url": "/18/static/js/main.8dba4829.chunk.js"
+  },
+  {
+    "revision": "55404e830124125a847c",
+    "url": "/18/static/js/runtime-main.45b0ada3.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/18/static/media/persik.4e1ec840.png"
+  }
+]);
